dab97f15f9f4efdc718fd599bcb9263518dccbbb
Alan Malloy <alan@malloys.org>
2011-03-05 18:26:52 -0800

    Use macrolet instead of globally defining a single-use macro

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject hot-potato "0.2.1"
   :description "A tool for re-ordering the arguments to functions (and other stuff too if you like)."
   :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]
                  [org.clojars.amalloy/utils "[0.1.0,)"]])
